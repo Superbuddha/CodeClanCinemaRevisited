316165f6fe8e834b9739cb66d21a981c2577ca05
Stephan Constantial <Sconstantial@yahoo.com>
2018-09-22 16:14:12 +0100

    cinema sql sorted

--- db/cinema.sql
+++ db/cinema.sql
@@ -16,6 +16,6 @@ CREATE TABLE customers(
 
 CREATE TABLE tickets(
   id SERIAL8 PRIMARY KEY,
-  film_id REFERENCES films(id) ON DELETE CASCADE
-  customer_id REFERENCES customers(id) ON DELETE CASCADE
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE
 );
